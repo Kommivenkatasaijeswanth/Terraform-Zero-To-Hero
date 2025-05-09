4c63667eca8e3a3eeb79f2aca413c4b41dd8522a
sam <jeswanth.k7891@gmail.com>
2025-05-08 22:43:17 -0700

    key pair changed

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "example" {
     ami           = "ami-0133f9d4af72da4d0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     subnet_id = "subnet-02d06eb34d1e0d7b7"
-    key_name = "jeswanth"
+    key_name = "jeswanthkommi"
 }
