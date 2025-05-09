f4118473bca7b3001682e2ac29e25978fd3b4894
sam <jeswanth.k7891@gmail.com>
2025-05-09 14:02:52 -0700

    subnet changed

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0133f9d4af72da4d0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
-    subnet_id = "subnet-02d06eb34d1e0d7b7"
+    subnet_id = "subnet-0bb82747e0f0ef68e"
     key_name = "jeswanthkommi"
 }
