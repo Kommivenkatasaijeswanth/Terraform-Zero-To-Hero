f57d41d220d97716a51cc062a2e8c5409949d4ee
sam <jeswanth.k7891@gmail.com>
2025-05-09 22:08:07 -0700

    Updated .tfvars, subnet_ID, AMI, assign IP address to true at output

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -1,9 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
+    key_name = var.key_name
+    associate_public_ip_address = true
+    tags = {
+      name = "sample"
+    }
 }
---
+++ Day-3/modules/ec2_instance/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value = "ami-0133f9d4af72da4d0"
+instance_type_value = "t2.micro"
+subnet_id_value = "subnet-0bbd95ea60bc997d3"
+key_name = "jeswanthkommi"
